b3e6d4ee0250ab25b3979d8a491fb9333704ebca
LucienRicome <145615046+LucienRicome@users.noreply.github.com>
2023-12-21 11:42:38 +0100

    Update 1_🧮_Math.py
    pourquoi comparer ces modèles ?

--- pages/1_🧮_Math.py
+++ pages/1_🧮_Math.py
@@ -19,6 +19,14 @@ st.subheader("Biomass and MSY biomass production equations with Schaefer and Fox
 
 cola, colb = st.columns(2)
 
+st.markdown("""
+            Le modèle de Schaefer décrit une croissance logistique de la population de Merlu. 
+            Ceci implique  un lien linéaire entre l'effort de pêche et le nombre de capture.
+            Quant à lui, le modèle de Fox décrit une croissance de Gompertz de la population (Fox, 1970). Cette croissance est plus lente 
+            au début et à la fin de la période de simulation. Elle a aussi pour conséquence la relation exponentielle 
+            entre le nombre de capture et l'effort de pêche (Fox 1970).   
+            """)
+
 with cola:
     
     # Equation du modèle de schaefer 
@@ -48,4 +56,11 @@ with colb:
     # CMSY with Fox model formalism
     
     #st.caption("L'équation du MSY issu du modèle de Fox")
-   
+
+st.markdown("""
+            Ces deux modèles décrivent des types de croissance différentes qui peuvent dépendre de la population étudiée.
+            Notons que Punt, A. E. (1992) a montré que le modèle de Fox est en moyenne plus fiable que le modèle de Schaefer pour décrire les
+            dynamiques halieutiques. Seulement, ce résultat dépend des populations étudiées. Dans certains cas, le modèle de Schaefer présente de meilleurs résultats
+            et inversemnt. Dans un premier temps, Il est donc important de comprendre la manière dont les paramètres r et K influencent les prédictions des modèles. 
+            Cette première approche devrait permettre un choix de modèle plus adapté à chaque situation.
+            """)
